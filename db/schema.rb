5e6483c109a8d3898705cf676462072cbcdc4ce9
aritode <aritode@users.noreply.github.com>
2020-03-03 19:49:52 +0300

    Add Null constraints to questions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_093640) do
+ActiveRecord::Schema.define(version: 2020_03_03_160830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
