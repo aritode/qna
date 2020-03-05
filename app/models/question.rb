9418c1f18850ec905452735f2a861b0eae6b972f
aritode <aritode@users.noreply.github.com>
2020-03-05 16:39:18 +0300

    Remove blank lines

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,7 @@
 class Question < ApplicationRecord
-
   has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
   validates :title, length: { minimum: 10, maximum: 255 }
   validates :body, length: { minimum: 10 }
-
 end
